8095ceb2d945d14d72b9bc1ce09b60fecd9be275
Ben Greenman <benjaminlgreenman@gmail.com>
2020-02-21 11:03:37 -0500

    LD: test define-typed/untyped-id
    have something that works for t/u-id --> transient
    
    but requires require/typed on the client side, bad!

--- typed-racket-test/LD/pass/untyped-contract-transient.rkt
+++ typed-racket-test/LD/pass/untyped-contract-transient.rkt
@@ -1,14 +1,12 @@
 #lang typed/racket #:locally-defensive
 
-;; Identifier imported from untyped-contract should have original type,
-;;  not the type for the refined contract
+;; Identifier imported from untyped-contract stuck with refined type
+;;  in a transient context.
+;; Need to deal with that refined type .... funny
 
-;; HANG ON this test must fail, because Transient needs a contract
-;;  and that contract should be the same as untyped --- because Transient is
-;;  a free pipeline to untyped
+(require "untyped-contract-aux/untyped.rkt")
 
-;(require "untyped-contract-aux/untyped.rkt")
-;
-;(define (g (z : (U Symbol String))) : Void
-;  (f z))
+(define (g (z : (U Symbol String))) : Void
+  ;; (f z) = type error
+  (f (assert z symbol?)))
 
