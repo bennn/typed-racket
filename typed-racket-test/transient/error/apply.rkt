5e04bc085dec7d758ebe11b34d8c958cea039508
Ben Greenman <benjaminlgreenman@gmail.com>
2020-03-27 12:12:31 -0400

    LD new apply regression test

---
+++ typed-racket-test/transient/error/apply.rkt
@@ -0,0 +1,14 @@
+#lang typed/racket/base #:transient
+
+;; Test that `(apply f ...)` checks results safely
+
+(module u racket/base
+  (provide f2)
+  (define (f2) (values 'a 'b)))
+
+(require/typed 'u
+  (f2 (-> (Values Integer Symbol))))
+
+(: b Integer)
+(: c Symbol)
+(define-values [b c] (apply f2 '()))
