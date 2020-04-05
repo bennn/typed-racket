fbed705902fe0a1f72c8453793ce9ae08d37d218
Ben Greenman <benjaminlgreenman@gmail.com>
2020-04-05 11:14:36 -0400

    LD fix zordoz typed-transient issue
    this was a NEW problem introduced when I changed the error messages,
     I put invalid syntax in the error case
    
      (error (x0 x1 x2) "message")
    
    Need `#%plain-app` or a quote at least

--- typed-racket-test/transient/pass/zordoz-opt.rkt
+++ typed-racket-test/transient/pass/zordoz-opt.rkt
@@ -3,8 +3,7 @@
 ;; Originally raised error in optimizer,
 ;;  expected kernel-literal (or something) got gensym var
 
-(require require-typed-check
-         racket/match)
+(require racket/match)
 
 (define-type result Symbol)
 (define-type zo Symbol)
@@ -18,7 +17,7 @@
 (define (find raw ctx hist pre-hist)
   (define arg "hello")
   (cond [(and arg (zo? ctx))
-         (define results (my-find ctx arg))
+         (define results : (Listof result) '())
          (match results
            ['()
             (values ctx hist pre-hist)]
@@ -26,8 +25,4 @@
             (values ctx '() pre-hist)])]
         [else
          (values ctx hist pre-hist)]))
-
-(: my-find (-> zo String [#:limit (U Natural #f)] (Listof result)))
-(define (my-find z str #:limit [lim #f])
-  '())
 
