9041e9769bd6dc62995f57d118410c88b6238c95
Ben Greenman <benjaminlgreenman@gmail.com>
2020-04-16 17:25:14 -0400

    LD undo sequence types
    previous "fix" caused other things to fail
    
    sadly the interesting parts of the type are on the return type,
     so we cant statically tell what type to use
    
    I'd added 2X new cases but that simply ignored the old ones
    
    need to find another way (hack) for transient to accept input ports

--- typed-racket-lib/typed-racket/base-env/base-special-env.rkt
+++ typed-racket-lib/typed-racket/base-env/base-special-env.rkt
@@ -79,18 +79,7 @@
   ;; make-sequence
   [(make-template-identifier 'make-sequence 'racket/private/for)
    (-poly (a b)
-          (let ([seq-vals3
-                 (lambda (a)
-                   (define pre-a (list Univ))
-                   (-values (list
-                             (-> Univ (-values pre-a))
-                             (Un (-> Univ Univ) (-val #f))
-                             (-> Univ Univ)
-                             Univ
-                             (Un (-> Univ Univ) (-val #f))
-                             (->* pre-a Univ : (-PS (-is-type 0 (car a)) -tt))
-                             (Un (->* (cons Univ pre-a) Univ) (-val #f)))))]
-                [seq-vals2
+          (let ([seq-vals
                  (lambda (a)
                    (-values (list
                              (-> Univ (-values a))
@@ -98,34 +87,16 @@
                              (-> Univ Univ)
                              Univ
                              (Un (-> Univ Univ) (-val #f))
-                             (-val #f)
-                             (Un (->* (cons Univ a) Univ) (-val #f)))))]
-                [seq-vals
-                 (lambda (a)
-                   (-values (list
-                             (-> Univ (-values a))
-                             (Un (-> Univ Univ) (-val #f))
-                             (-> Univ Univ)
-                             Univ
-                             (Un (-> Univ Univ) (-val #f))
-                             (Un (->* a Univ Univ) (-val #f))
-                             (Un (->* (cons Univ a) Univ) (-val #f)))))]
-                )
+                             (Un (->* a Univ) (-val #f))
+                             (Un (->* (cons Univ a) Univ) (-val #f)))))])
             (cl->*
              (-> Univ -Byte         (seq-vals (list -Byte)))
-            ;  (-> Univ -Byte         (seq-vals2 (list -Byte)))
              (-> Univ -Index        (seq-vals (list -Index)))
-            ;  (-> Univ -Index        (seq-vals2 (list -Index)))
              ;; Generous. Negative numbers aren't allowed.
              (-> Univ -Fixnum       (seq-vals (list -NonNegFixnum)))
-            ;  (-> Univ -Fixnum       (seq-vals2 (list -NonNegFixnum)))
              (-> Univ -Int          (seq-vals (list -Nat)))
-            ;  (-> Univ -Int          (seq-vals2 (list -Nat)))
              (-> Univ (-seq a) (seq-vals (list a)))
-            ;  (-> Univ (-seq a) (seq-vals2 (list a)))
-             (-> Univ (-seq a b) (seq-vals (list a b)))
-            ;  (-> Univ (-seq a b) (seq-vals2 (list a b)))
-            )))]
+             (-> Univ (-seq a b) (seq-vals (list a b))))))]
   ;; check-range
   [(make-template-identifier 'check-range 'racket/private/for)
    (-> Univ Univ Univ -Void)]
