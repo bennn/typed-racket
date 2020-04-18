2890a286737ec8ebc051d9fd3ccc9b1ff1a07388
Ben Greenman <benjaminlgreenman@gmail.com>
2020-04-18 10:10:15 -0400

    LD debug module+ issue ... have work-around
    turns out, each submodule runs independently
    
    so a #:transient module has #:guarded submodules by default;
     for `module+` and `module* _ #f` this is inintuitive
    
    work-around = add the keyword
    
    maybe adding a #lang for transient will make it possible to fix this
    ... set the te-mode earlier, so that `mb-core` can read it

---
+++ typed-racket-test/transient/pass/module-plus.rkt
@@ -0,0 +1,18 @@
+#lang typed/racket/base #:transient
+
+;; Make sure defender runs in module+
+;;  ... ideally WITHOUT the #:transient flag, but alas
+
+(module u racket/base
+  (define b
+    (box "hello"))
+  (provide b))
+
+(require/typed 'u (b (Boxof Integer)))
+
+(module+ test
+  #:transient
+  (require typed/rackunit)
+
+  (check-exn exn:fail:contract?
+    (lambda () (unbox b))))
