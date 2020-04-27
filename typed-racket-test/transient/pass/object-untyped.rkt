f2e2f1e5bc9e1b3cf33f472f99300b91fc61b247
Ben Greenman <benjaminlgreenman@gmail.com>
2020-04-27 18:42:03 -0400

    LD more accessor / selector tests, need to fix
    tests fail currently, need to improve implementation

---
+++ typed-racket-test/transient/pass/object-untyped.rkt
@@ -0,0 +1,23 @@
+#lang typed/racket #:transient
+
+(module u racket
+  (define c%
+    (class object%
+      (super-new)
+      (define/public (f x) '(+ x 1))))
+  (provide c%))
+
+(define-type C% (Class (f (-> Integer Integer))))
+
+(require typed/rackunit)
+(require/typed (submod "." u)
+  (c% C%))
+
+(: g (-> (Vector (Instance C%)) Integer))
+(define (g vo)
+  (define o (vector-ref vo 0))
+  (send o f (send o f 2)))
+
+(check-exn exn:fail:contract?
+  (lambda () (g (vector (new c%)))))
+
