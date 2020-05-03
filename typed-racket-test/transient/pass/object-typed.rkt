4c13986861ae43fd9fd0043f5d1c2eda1279f2ba
Ben Greenman <benjaminlgreenman@gmail.com>
2020-05-03 12:12:29 -0400

    LD add object init-field test
    fields do not need defense

--- typed-racket-test/transient/pass/object-typed.rkt
+++ typed-racket-test/transient/pass/object-typed.rkt
@@ -1,18 +1,21 @@
 #lang racket
 
 (module t typed/racket #:transient
-  (define-type C% (Class (f (-> Integer Integer))))
+  (define-type C% (Class (init-field (s String)) (f (-> Integer Integer))))
   (: c% C%)
   (define c%
     (class object%
       (super-new)
-      (define/public (f x) x)))
+      (init-field s)
+      (define/public (f x) (+ 4 4))))
   (provide c%))
 
 (require 't rackunit)
 
-(define o (new c%))
+(define o (new c% (s "hello")))
 
 (check-exn exn:fail:contract?
   (lambda () (send o f 'NaN)))
 
+(check-not-exn
+  (lambda () (new c% (s 'NotString))))
