1e6ccd7319175c11355639513723b26969c0dfe9
Ben Greenman <benjaminlgreenman@gmail.com>
2020-05-04 20:51:32 -0400

    LD defend more optional + keyword procs

--- typed-racket-test/transient/pass/flonum.rkt
+++ typed-racket-test/transient/pass/flonum.rkt
@@ -1,10 +1,18 @@
-#lang typed/racket/base #:transient
+#lang racket/base
 
-;; Test case-> type
+(module t typed/racket/base #:transient
+  ;; Test case-> type
+  (require racket/flonum)
+  (provide flprobability?)
+  (: flprobability? (case-> (Flonum -> Boolean) (Flonum Any -> Boolean)))
+  (define (flprobability? p [log? #f])
+    (cond [log?  (and (p . fl>= . -inf.0) (p . fl<= . 0.0))]
+          [else  (and (p . fl>= . 0.0) (p . fl<= . 1.0))])))
 
-(require racket/flonum)
+(require 't rackunit)
 
-(: flprobability? (case-> (Flonum -> Boolean) (Flonum Any -> Boolean)))
-(define (flprobability? p [log? #f])
-  (cond [log?  (and (p . fl>= . -inf.0) (p . fl<= . 0.0))]
-        [else  (and (p . fl>= . 0.0) (p . fl<= . 1.0))]))
+(check-not-exn
+  (lambda () (flprobability? 0.5)))
+
+(check-exn #rx"transient-assert"
+  (lambda () (flprobability? "seven")))
