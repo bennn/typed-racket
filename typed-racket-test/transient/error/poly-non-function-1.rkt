92ee714d25b669fb160da9083e18845737be7ff2
Ben Greenman <benjaminlgreenman@gmail.com>
2020-08-02 19:33:24 -0400

    LD: new bad All example

---
+++ typed-racket-test/transient/error/poly-non-function-1.rkt
@@ -0,0 +1,16 @@
+#lang typed/racket/base #:transient
+
+;; Expected: contract generation error
+;;
+;; To remove the error, we need to treat `inst` as an elimination form
+;; when the poly type does not guarantee some kind of shape.
+
+(require/typed racket/base
+  (cdr (All (A) (U (Boxof A) A))))
+
+(let ((v : (U (Boxof (Pairof String String)) (Pairof String String))
+       (inst cdr (Pairof String String))))
+  (if (box? v)
+    #f
+    (car v)))
+
